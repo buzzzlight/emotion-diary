8e808015dd3434316fced1dbb177901c864a5912
김수빈 <pomodoro5292@gmail.com>
2023-07-19 23:07:58 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
+function App() {
+  return (
+    // 페이지 라우팅
+    <BrowserRouter>
+      <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
+        <h2>App.js</h2>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/New.js
@@ -0,0 +1,10 @@
+const New = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>이곳은 일기 작성 페이지 입니다.</p>
+    </div>
+  );
+};
+
+export default New;
